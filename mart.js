cdf2a391277f3b198061f15eb5e1fefcbee1d62e
Asif Mustafayev <asif.mustafayev.13@gmail.com>
2020-12-23 16:30:43 +0400

    8MArch

---
+++ mart.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+  $(function(){
+    var string="Happy womens day!!!!March 8";
+    var q=jQuery.map(string.split(''),function(letter){
+      return $('<span>'+letter + '</span>');
+    });
+    var dest=$('#ghost');
+    var c=0;
+    var i=setInterval(function(){
+      q[c].appendTo(dest).hide().fadeIn(900);
+      c +=1;
+      if(c >= q.length)
+        clearInterval(i);},200);
+    });
+    
+  
+}
